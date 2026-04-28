be7be933d12013fed0f1bfd1fba637ca0b14d770
Karan-Developer-69 <homepc8890@gmail.com>
2026-04-28 19:19:35 +0530

    Git hub user fteching 90%

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,4 +1,8 @@
 import axios from "axios";
+import qs from "querystring";
+import {config} from "dotenv";
+
+config();
 
 const GITHUB_CLIENT_ID = process.env.GITHUB_CLIENT_ID;
 const GITHUB_CLIENT_SECRET = process.env.GITHUB_CLIENT_SECRET;
@@ -6,40 +10,45 @@ const GITHUB_CLIENT_SECRET = process.env.GITHUB_CLIENT_SECRET;
 const githubAuth = (req, res) => {
     // Implement GitHub OAuth logic here
     res.send(`
-        <a href="https://github.com/login/oauth/authorize?client_id=${process.env.GITHUB_CLIENT_ID}" >Login with GitHub</a>
+        <a href="https://github.com/login/oauth/authorize?client_id=${process.env.GITHUB_CLIENT_ID}&scope=user">Login with GitHub</a>
         `);
 }
 
-import qs from "querystring";
 
 const githubcallback = async (req, res) => {
     try {
         const { code } = req.query;
 
-        console.log("CODE:", code);
-
         const response = await axios.post(
             "https://github.com/login/oauth/access_token",
             qs.stringify({
-                client_id: process.env.CLIENT_ID,
-                client_secret: process.env.CLIENT_SECRET,
-                code,
+                client_id:GITHUB_CLIENT_ID,
+                client_secret:GITHUB_CLIENT_SECRET,
+                code: code,
             }),
             {
                 headers: {
-                    Accept: "application/json",
+                    "Accept": "application/json",
                     "Content-Type": "application/x-www-form-urlencoded",
                 },
             }
         );
 
-        console.log("TOKEN RESPONSE:", response.data);
+         const accessToken = await response.data.access_token;
+            console.log("Access Token:", accessToken);
+         const userRes = await axios.get("https://api.github.com/user", {
+            headers: {
+            Authorization: `Bearer ${accessToken}`
+            }
+         });
+         console.log("User Response:", userRes.data);
+         const user = await userRes.data;
 
-        res.json(response.data);
+         res.json(user);
 
     } catch (error) {
-        console.log("FULL ERROR:", error.response?.data || error.message);
-        res.status(500).json(error.response?.data || "Internal error");
+        console.log("ERROR:", error.response?.data || error.message);
+        res.status(500).json(error.response?.data);
     }
 };
 
